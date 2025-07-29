9589b92f2efa331f696117b3808d09686cded44a
Artemiy Kvyatkovskiy <artemy.kvyatkovsky@avangard-mb.ru>
2025-07-29 17:45:43 +0300

    Updates

---
+++ assets/CyIMy9op.js
@@ -0,0 +1,1 @@
+import{_ as e,c as r,o as c}from"./_QTx47lI.js";const o={};function t(n,s){return c(),r("hr")}const a=e(o,[["render",t]]);export{a as default};
---
+++ assets/DPu2BS42.js
@@ -0,0 +1,1 @@
+import{_ as e,c as _,o as c}from"./_QTx47lI.js";const n={};function o(r,t){return c(),_("h1",null,"SLUG PAGE")}const a=e(n,[["render",o]]);export{a as default};
